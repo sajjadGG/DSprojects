f7fe78ec62f80bb6eb17dcf0d434e824315dbcba
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-02-01 21:33:47 +0330

    add heap class and its unit test

--- huffmanEncoding/huffman.py
+++ huffmanEncoding/huffman.py
@@ -3,6 +3,9 @@ implement a module for huffman encoding and decoding
 """
 
 class Stack():
+    """
+    Stack implementation using list
+    """
 
     def __init__(self):
         self.data=[]
@@ -22,10 +25,30 @@ class Node():
         def __init__(self,data,key=None,parent=None , left=None , right=None):
             self.data = data
             if key==None:
-                key=data
+                self.key=data
+            else:
+                self.key = key
             self.parent = parent
             self.left = left
             self.right = right
+
+        def get_data(self):
+            return self.data
+
+        def get_key(self):
+            return self.key
+
+        def set_data(self , data):
+            self.data = data
+        
+        def set_key(self , key):
+            self.key = self.key
+
+        def get_left(self):
+            return self.left
+
+        def get_right(self):
+            return self.right
 
         def is_root(self):
             return self.parent==None
@@ -34,107 +57,150 @@ class Node():
             return self.left==None and self.right==None
         
         def __str__(self):
-            return str(data)
+            return str(self.data)
+
+        def __gt__(self , other):
+            return self.key > other.key
+
+        def __lt__(self , other):
+            return self.key < other.key
+
+        def __eq__(self , other):
+            return self.key == other.key
+
+        def __le__(self , other):
+            return self.key <= other.key
+
+        def __ge__(self , other):
+            return self.key >= other.key
 
         def shallow_copy(self):
             return Node(self.data , self.key ,  self.parent , self.left , self.right)
 
         def deep_copy(self):
-            def _copy(node,parent=None):
-                if node == None:
-                    return None
-                else:
-                    res = Node(node.date , node.key , parent)
-                    left = _copy(node.left , res)
-                    right = _copy(node.right , res)
-                    res.left = left
-                    res.right = right
-                    return res
-            #TODO : assumption the self node is the root
-            return _copy(self)
-
-class BinaryTree():
-    
-   
-        
-    def __init__(self,root,policy='max_heap'):
-        self.root = root
-        if isinstance(policy,str):
-            #TODO : support for 'min_heap' , 'BST'
-            if policy in ['max_heap']:
-                self.prop_func = self._max_heap_prop
-
-            self.policy = policy
-        else:
-            self.prop_func = policy
-            self.policy='custom' 
-    
-    def traverse(self , how='in_order'):
-        
-        if how=='in_order':
-            _in_order(self.root)
-        elif how=='pre_order':
-            _pre_order(self.root)
-        elif how=='post_order':
-            _pre_order(self.root)
-        else:
-            print("how specifed is not in_order , pre_order or post_order please specifie one of these")
-        
-
-    def add(self,data,key=None):
-        
-        n = Node(data,key=key,left=self.root)
-        self.root.parent = n
-        self.root=n
-        if self.check_prop(self.root):
-            return
-        else:
-            #roll back
             pass
-            
-    def check_prop(self,root):
-        return self.prop_func(root)
 
     
-    #private methods
-    def _in_order(self,node):
-        if node==None:
-            return
-        else:
-            _in_order(node.left)
-            print(node)
-            _in_order(node.right)
-    
-    def _post_order(self,node):
-        if node==None:
-            return
-        else:
-            _in_order(node.left)
-            _in_order(node.right)
-            print(node)
-    
-    def _pre_order(self,node):
-        if node==None:
-            return
-        else:
-            print(node)
-            _in_order(node.left)
-            _in_order(node.right)
-    
-    def _max_heap_prop(self,n):
-        return (n.key > (n.left.key if n.left!=None else n.key-1)) and (n.key > (n.right.key if n.right!=None else n.key-1) )
+import sys 
+
+class MaxHeap: 
+
+	def __init__(self, maxsize=100 , min_key=-100): 
+		
+		self.maxsize = maxsize 
+		self.size = 0
+		self.Heap = [min_key] * (self.maxsize + 1) 
+		self.Heap[0] = Node(sys.maxsize )
+		self.FRONT = 1
+
+	# Function to return the position of 
+	# parent for the node currently 
+	# at pos 
+	def parent(self, pos): 
+		
+		return pos // 2
+
+	# Function to return the position of 
+	# the left child for the node currently 
+	# at pos 
+	def leftChild(self, pos): 
+		
+		return 2 * pos 
+
+	# Function to return the position of 
+	# the right child for the node currently 
+	# at pos 
+	def rightChild(self, pos): 
+		
+		return (2 * pos) + 1
+
+	# Function that returns true if the passed 
+	# node is a leaf node 
+	def isLeaf(self, pos): 
+		
+		if pos >= (self.size//2) and pos <= self.size: 
+			return True
+		return False
+
+	# Function to swap two nodes of the heap 
+	def swap(self, fpos, spos): 
+		
+		self.Heap[fpos], self.Heap[spos] = (self.Heap[spos], 
+											self.Heap[fpos]) 
+
+	# Function to heapify the node at pos 
+	def maxHeapify(self, pos): 
+
+		# If the node is a non-leaf node and smaller 
+		# than any of its child 
+		if not self.isLeaf(pos): 
+			if (self.Heap[pos] < self.Heap[self.leftChild(pos)] or
+				self.Heap[pos] < self.Heap[self.rightChild(pos)]): 
+
+				# Swap with the left child and heapify 
+				# the left child 
+				if (self.Heap[self.leftChild(pos)] > 
+					self.Heap[self.rightChild(pos)]): 
+					self.swap(pos, self.leftChild(pos)) 
+					self.maxHeapify(self.leftChild(pos)) 
+
+				# Swap with the right child and heapify 
+				# the right child 
+				else: 
+					self.swap(pos, self.rightChild(pos)) 
+					self.maxHeapify(self.rightChild(pos)) 
+
+	# Function to insert a node into the heap 
+	def insert(self, element): 
+		
+		if self.size >= self.maxsize: 
+			return
+		self.size += 1
+		self.Heap[self.size] = element 
+
+		current = self.size 
+
+		while (self.Heap[current] > 
+			self.Heap[self.parent(current)]): 
+			self.swap(current, self.parent(current)) 
+			current = self.parent(current) 
+
+	# Function to print the contents of the heap 
+	def Print(self): 
+		
+		for i in range(1, (self.size // 2) + 1): 
+			print(" PARENT : " + str(self.Heap[i]) +
+				" LEFT CHILD : " + str(self.Heap[2 * i]) +
+				" RIGHT CHILD : " + str(self.Heap[2 * i + 1])) 
+
+	# Function to remove and return the maximum 
+	# element from the heap 
+	def extractMax(self): 
+
+		popped = self.Heap[self.FRONT] 
+		self.Heap[self.FRONT] = self.Heap[self.size] 
+		self.size -= 1
+		self.maxHeapify(self.FRONT) 
+		
+		return popped 
+
+print('The maxHeap is ') 
+      
+maxHeap = MaxHeap(15) 
+maxHeap.insert(Node(5)) 
+maxHeap.insert(Node(3)) 
+maxHeap.insert(Node(17)) 
+maxHeap.insert(Node(10)) 
+maxHeap.insert(Node(84)) 
+maxHeap.insert(Node(19)) 
+maxHeap.insert(Node(6)) 
+maxHeap.insert(Node(22)) 
+maxHeap.insert(Node(9)) 
+
+maxHeap.Print() 
 
 
-    def copy(self):
-        def _copy(node):
-            if node == None:
-                return None
-            else:
-                return node.shallow_copy()
 
-        return BinaryTree(_copy(root))
-
-    
 class HuffmanEndoder():
     """
     HuffmanEncoder for .txt file and decode .cmp files
