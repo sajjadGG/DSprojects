6b3519b7ed5536ec04e28dd9f40e7c9f63797a44
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-02-01 23:20:32 +0330

    add enocode function in huffman class

--- huffmanEncoding/huffman.py
+++ huffmanEncoding/huffman.py
@@ -51,10 +51,10 @@ class Node():
             return self.right
 
         def is_root(self):
-            return self.parent==None
+            return self.parent is None
 
         def is_leaf(self):
-            return self.left==None and self.right==None
+            return self.left is None and self.right is None
         
         def __str__(self):
             return str(self.data)
@@ -73,6 +73,19 @@ class Node():
 
         def __ge__(self , other):
             return self.key >= other.key
+        
+        def print_down(self):
+            if self is None:
+                return
+            if self.is_leaf():
+                print(self)
+            else:
+                if self.left is not None:
+                    self.left.print_down()
+                print(self)
+                if self.right is not None:
+                    self.right.print_down()
+
 
         def shallow_copy(self):
             return Node(self.data , self.key ,  self.parent , self.left , self.right)
@@ -80,134 +93,16 @@ class Node():
         def deep_copy(self):
             pass
 
-    
-import sys 
-
-class MaxHeap: 
-
-	def __init__(self, maxsize=100 , min_key=-100): 
-		
-		self.maxsize = maxsize 
-		self.size = 0
-		self.Heap = [min_key] * (self.maxsize + 1) 
-		self.Heap[0] = Node(sys.maxsize )
-		self.FRONT = 1
-
-	# Function to return the position of 
-	# parent for the node currently 
-	# at pos 
-	def parent(self, pos): 
-		
-		return pos // 2
-
-	# Function to return the position of 
-	# the left child for the node currently 
-	# at pos 
-	def leftChild(self, pos): 
-		
-		return 2 * pos 
-
-	# Function to return the position of 
-	# the right child for the node currently 
-	# at pos 
-	def rightChild(self, pos): 
-		
-		return (2 * pos) + 1
-
-	# Function that returns true if the passed 
-	# node is a leaf node 
-	def isLeaf(self, pos): 
-		
-		if pos >= (self.size//2) and pos <= self.size: 
-			return True
-		return False
-
-	# Function to swap two nodes of the heap 
-	def swap(self, fpos, spos): 
-		
-		self.Heap[fpos], self.Heap[spos] = (self.Heap[spos], 
-											self.Heap[fpos]) 
-
-	# Function to heapify the node at pos 
-	def maxHeapify(self, pos): 
-
-		# If the node is a non-leaf node and smaller 
-		# than any of its child 
-		if not self.isLeaf(pos): 
-			if (self.Heap[pos] < self.Heap[self.leftChild(pos)] or
-				self.Heap[pos] < self.Heap[self.rightChild(pos)]): 
-
-				# Swap with the left child and heapify 
-				# the left child 
-				if (self.Heap[self.leftChild(pos)] > 
-					self.Heap[self.rightChild(pos)]): 
-					self.swap(pos, self.leftChild(pos)) 
-					self.maxHeapify(self.leftChild(pos)) 
-
-				# Swap with the right child and heapify 
-				# the right child 
-				else: 
-					self.swap(pos, self.rightChild(pos)) 
-					self.maxHeapify(self.rightChild(pos)) 
-
-	# Function to insert a node into the heap 
-	def insert(self, element): 
-		
-		if self.size >= self.maxsize: 
-			return
-		self.size += 1
-		self.Heap[self.size] = element 
-
-		current = self.size 
-
-		while (self.Heap[current] > 
-			self.Heap[self.parent(current)]): 
-			self.swap(current, self.parent(current)) 
-			current = self.parent(current) 
-
-	# Function to print the contents of the heap 
-	def Print(self): 
-		
-		for i in range(1, (self.size // 2) + 1): 
-			print(" PARENT : " + str(self.Heap[i]) +
-				" LEFT CHILD : " + str(self.Heap[2 * i]) +
-				" RIGHT CHILD : " + str(self.Heap[2 * i + 1])) 
-
-	# Function to remove and return the maximum 
-	# element from the heap 
-	def extractMax(self): 
-
-		popped = self.Heap[self.FRONT] 
-		self.Heap[self.FRONT] = self.Heap[self.size] 
-		self.size -= 1
-		self.maxHeapify(self.FRONT) 
-		
-		return popped 
-
-print('The maxHeap is ') 
-      
-maxHeap = MaxHeap(15) 
-maxHeap.insert(Node(5)) 
-maxHeap.insert(Node(3)) 
-maxHeap.insert(Node(17)) 
-maxHeap.insert(Node(10)) 
-maxHeap.insert(Node(84)) 
-maxHeap.insert(Node(19)) 
-maxHeap.insert(Node(6)) 
-maxHeap.insert(Node(22)) 
-maxHeap.insert(Node(9)) 
-
-maxHeap.Print() 
 
 
-
+from heapq import *
 class HuffmanEndoder():
     """
     HuffmanEncoder for .txt file and decode .cmp files
 
     """
     def __init__(self):
-        
+        pass
 
     def _freq(self , path):
         #TODO : exception handling
@@ -222,13 +117,61 @@ class HuffmanEndoder():
         return freqs
 
     def _build_tree(self , char_freq):
+        pad = 10
+        self.heap = []
+        for k in char_freq:
+            heappush(self.heap , (char_freq[k] , Node(k , char_freq[k])))
         
-        pass
+    
+    def _extract_codes(self , root):
+        path=""
+        codes = {}
+        def _extract(node , path):
+            if node is None:
+                return
+            if node.is_leaf():
+                codes[node.data] = path
+            else:
+                _extract(node.left , path+'0')
+                _extract(node.right , path+'1')
+        _extract(root , "")
+        return codes
 
-    def encode(self,path):
+
+    def _write_file(self,read_path,write_path):
+
+        with open(read_path , 'r') as rf , open(write_path , 'w') as wf:
+            for l in rf.readlines():
+                for c in l:
+                    wf.write(self.codes[c])
+        
+        
 
 
-        pass
+    #Based on CLR
+    def encode(self,path, write_path='enc.cmd'):
+        
+        freqs=self._freq(path)
+        n = len(freqs)
+        self._build_tree(freqs)
+        for i in range(1,n):
+            #TODO : exception heap out of range
+            print('hi')
+            left = heappop(self.heap)[1]
+            right = heappop(self.heap)[1]
+            z = Node(left.data + right.data , left.key + right.key , left=left , right=right)
+            print(z)
+            heappush(self.heap,(z.key , z))
+
+        self.codes = self._extract_codes(heappop(self.heap)[1])
+        print(self.codes)
+        self._write_file(path , write_path)
+
+        
 
     def decode(self,path):
         pass
+
+h =HuffmanEndoder()
+root = h.encode('test.txt')
+# root.print_down() 
