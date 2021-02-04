bf41958ff89188caa15f5cd731348477916f3a50
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-02-04 11:46:29 +0330

    add decode necessary information to header of the file

--- huffmanEncoding/huffman.py
+++ huffmanEncoding/huffman.py
@@ -1,5 +1,11 @@
 """
 implement a module for huffman encoding and decoding
+endcode into cmp file first write the decode dictionary needed for decoding 
+then write the data
+for instance if the input is "h" and code is h : 0 encoded file will be
+h:0
+
+0
 """
 
 class Stack():
@@ -105,6 +111,14 @@ class HuffmanEndoder():
         pass
 
     def _freq(self , path):
+        """[summary]
+            extract character frequencies fromfile
+        Args:
+            path (str): [description]. file to be decoded
+
+        Returns:
+            (dict): [description]. char :  frequency
+        """
         #TODO : exception handling
         freqs = {}
         with open(path , 'r') as f:
@@ -116,14 +130,26 @@ class HuffmanEndoder():
                     freqs[c]=1
         return freqs
 
-    def _build_tree(self , char_freq):
-        pad = 10
+    def _build_heap(self , char_freq):
+        """[summary]
+            build heap using character frequencies
+        Args:
+            char_freq (dict): [description]. char : freq
+        """
         self.heap = []
         for k in char_freq:
             heappush(self.heap , (char_freq[k] , Node(k , char_freq[k])))
         
     
     def _extract_codes(self , root):
+        """[summary]
+            extract encoding codes from huffman tree
+        Args:
+            root (Node): [description]. root of huffman tree
+
+        Returns:
+            (dict): [description]. char : huffmancode
+        """
         path=""
         codes = {}
         def _extract(node , path):
@@ -139,8 +165,24 @@ class HuffmanEndoder():
 
 
     def _write_file(self,read_path,write_path):
-
+        """[summary]
+            write encoded file
+        Args:
+            read_path (str): [description]. file to be encoded
+            write_path (str): [description]. path to write encoded file
+        """
+        decodes = self._decode_dic()
+        n = len(decodes)
         with open(read_path , 'r') as rf , open(write_path , 'w') as wf:
+            i = 0
+            for k in decodes:
+                wf.write("{}:{}".format(k,decodes[k]))
+                i+=1
+                if i<n:
+                    wf.write(',')
+                else:
+                    wf.write('\n')
+
             for c in rf.read():
                 wf.write(self.codes[c])
         
@@ -149,10 +191,15 @@ class HuffmanEndoder():
 
     #Based on CLR
     def encode(self,path, write_path='enc.cmp'):
-        
+        """[summary]
+            public interface for encoding the file
+        Args:
+            path (str): [description] path to file which is requested to be encoded 
+            write_path (str, optional): [description]. Defaults to 'enc.cmp'.
+        """
         freqs=self._freq(path)
         n = len(freqs)
-        self._build_tree(freqs)
+        self._build_heap(freqs)
         for i in range(1,n):
             #TODO : exception heap out of range
             print('hi')
@@ -168,20 +215,52 @@ class HuffmanEndoder():
 
         
     def _decode_dic(self):
+        """[summary]
+        extract revese dictionary for decoding
+        Returns:
+            (dict): [description]. huffmancode  : char
+        """
         assert self.codes is not None
         decodes = {}
         for k in self.codes:
             decodes[self.codes[k]] = k
         return decodes
 
-    def decode(self,path ,decodes=None, write_path='decode.txt'):
-        if decodes is None:
-            decodes = self._decode_dic()
+    def _extract_decode(self , c):
+        """[summary]
+        extract decode dictionary from file meta data in files header
+        Args:
+            c (str): header of the file consist of information needed to 
+            construct decode dictionary
 
+        Returns:
+            (dict) : [description]. huffmancode : char
+        """
+        l =  c.split(',')
+        de = {}
+        for e in l:
+            k,v = e.split(':')
+            de[k] = v
+        return de
+
+
+    def decode(self,path , write_path='decode.txt'):
+        """[summary]
+            decode a file in format specified by the this module
+        Args:
+            path (str): [description]. path of file to be decoded
+            write_path (str, optional): [description]. Defaults to 'decode.txt'.
+        """
+        
+        is_data=False
         with open(path , 'r') as rf , open(write_path , 'w') as wf:
+            
             l = rf.read()
+            decodes = self._extract_decode(l.split('\n')[0])
+
             res=""
             curr=0
+            l = l.split('\n')[1]
             while curr<len(l):
                 if res in decodes:
                     wf.write(decodes[res])
@@ -193,7 +272,21 @@ class HuffmanEndoder():
         
         
 
-h =HuffmanEndoder()
-root = h.encode('test.txt')
-h.decode('enc.cmp')
+if __name__ =='__main__':
+
+    h =HuffmanEndoder()
+    print("please enter your file path")
+    l = input()
+    inp_path , extension = "".join(l.split('.')[:-1]) , l.split('.')[-1]
+
+    if extension=='txt':
+        h.encode(l , inp_path+'.cmp')
+        print("done")
+
+    elif extension=='cmp':
+        h.decode(inp_path , inp_path+'decoded.txt')
+        
+
+# root = h.encode('test.txt')
+# h.decode('enc.cmp')
 # root.print_down() 
