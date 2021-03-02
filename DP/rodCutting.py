d65bbed22767d76a6e3046a9aae7cf187c744700
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-03-02 12:57:39 +0330

    feat(rodCutting): construct solution in dp

--- DP/rodCutting.py
+++ DP/rodCutting.py
@@ -14,6 +14,11 @@ def max_revenue_rec(n , prices):
                 maxq = candidq
         return candidq
 
+def calc_sol(sol , n , path=''):
+    if n not in sol:
+        return ''
+    else:
+        return "{}-{}-{}".format(calc_sol(sol , sol[n]-1) ,sol[n] , calc_sol(sol , n-sol[n]+1))
 
 
 def max_revenue_dp(n , prices):
@@ -23,14 +28,18 @@ def max_revenue_dp(n , prices):
         return prices[0]
     else:
         dp={0:0,1:prices[0]}
+        sol = {0:[] , 1:[1]}
         for k in range(2,n+1):
             maxq = prices[k-1]
+            c = [k]
             for i in range(1,k):
                 candidq = dp[k-i] + dp[i]
                 if candidq> maxq:
                     maxq = candidq
+                    c = sol[k-i] + sol[i]
             dp[k]=maxq
-    return dp[n]
+            sol[k] = c
+    return dp[n] , sol[n]
 
-print(max_revenue_dp(10 , prices=[1,5,8,9,10,17,17,20,24,30]))
+print(max_revenue_dp(10 , prices=[10,5,8,9,20,17,17,20,24,30]))
 
